8fb9c517524a23903bb62e0c33feb016ebdf19d5
Arteminariy <artemborisoff.ab@gmail.com>
2023-08-18 18:22:35 +0300

    add: AboutPage

---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from './AboutPage.module.scss'
+ 
+interface IAboutPageProps {}
+ 
+const AboutPage: FC<IAboutPageProps> = () => {
+	return (
+		<div className={styles.AboutPageWrapper}>
+			
+		</div>
+	)
+}
+ 
+export default AboutPage
