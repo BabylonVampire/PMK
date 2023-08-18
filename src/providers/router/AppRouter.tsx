b407937e19249898424aa21330374e09f747880c
Arteminariy <artemborisoff.ab@gmail.com>
2023-08-18 18:07:05 +0300

    add: async pages loading

--- src/providers/router/AppRouter.tsx
+++ src/providers/router/AppRouter.tsx
@@ -1,12 +1,12 @@
 import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import MainPage from '../../pages/MainPage/MainPage';
+import { MainPage } from '../../pages/MainPage';
 
 const AppRouter: FC = () => {
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<Routes>
-				<Route path="/" element={<MainPage/>} />
+				<Route path="/" element={<MainPage />} />
 			</Routes>
 		</Suspense>
 	);
