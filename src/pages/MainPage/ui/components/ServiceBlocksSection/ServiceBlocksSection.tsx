772c1c993e3470970b7eddae6a7b1370d8bee1aa
Arteminariy <artemborisoff.ab@gmail.com>
2023-08-18 19:13:41 +0300

    fix: gallery

--- src/pages/MainPage/ui/components/ServiceBlocksSection/ServiceBlocksSection.tsx
+++ src/pages/MainPage/ui/components/ServiceBlocksSection/ServiceBlocksSection.tsx
@@ -10,37 +10,12 @@ interface IServiceBlocksSectionProps {
 const ServiceBlocksSection: FC<IServiceBlocksSectionProps> = ({ blocks }) => {
 	return (
 		<div className={styles.serviceBlocks} id="serviceBlocks">
-			{blocks.map((block) => (
-				<Block block={block}/>
-			))}
-			{/* <div className={styles.galleryLeft}>
-				<Block
-					block={{
-						id: 1,
-						title: 'asd',
-						description: 'asd',
-						image: 'https://sun9-76.userapi.com/impg/K_6EG6wRcrM2OGKN69LC_4GBAWYIk0YjljEPSw/Aic60Xo5KTc.jpg?size=1600x900&quality=96&sign=0ab0c4a0ee9f617a260ccb0bf5a96b3b&type=album',
-					}}
-				/>
-				<Block
-					block={{
-						id: 3,
-						title: 'asd',
-						description: 'asd',
-						image: 'https://sun9-76.userapi.com/impg/K_6EG6wRcrM2OGKN69LC_4GBAWYIk0YjljEPSw/Aic60Xo5KTc.jpg?size=1600x900&quality=96&sign=0ab0c4a0ee9f617a260ccb0bf5a96b3b&type=album',
-					}}
-				/>
-			</div>
-			<div className={styles.galleryRight}>
-				<Block
-					block={{
-						id: 2,
-						title: 'asd',
-						description: 'asd',
-						image: 'https://sun9-76.userapi.com/impg/K_6EG6wRcrM2OGKN69LC_4GBAWYIk0YjljEPSw/Aic60Xo5KTc.jpg?size=1600x900&quality=96&sign=0ab0c4a0ee9f617a260ccb0bf5a96b3b&type=album',
-					}}
-				/>
-			</div> */}
+			<div className={styles.galleryLeft}>{blocks.filter((block) => block.id % 2 === 1).map((block) => {
+				return <Block block={block} />
+			})}</div>
+			<div className={styles.galleryRight}>{blocks.filter((block) => block.id % 2 === 0).map((block) => {
+				return <Block block={block} />
+			})}</div>
 		</div>
 	);
 };
