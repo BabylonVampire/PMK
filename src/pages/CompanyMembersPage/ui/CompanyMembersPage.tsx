dc20f1710ba5c55147807f2c3a5651f94e5667b8
Arteminariy <artemborisoff.ab@gmail.com>
2023-08-18 18:45:26 +0300

    add: company members page

---
+++ src/pages/CompanyMembersPage/ui/CompanyMembersPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import styles from './CompanyMembersPage.module.scss'
+ 
+interface ICompanyMembersPageProps {}
+ 
+const CompanyMembersPage: FC<ICompanyMembersPageProps> = () => {
+	return (
+		<div className={styles.CompanyMembersPageWrapper}>
+			
+		</div>
+	)
+}
+ 
+export default CompanyMembersPage
--- src/providers/router/AppRouter.tsx
+++ src/providers/router/AppRouter.tsx
@@ -1,8 +1,9 @@
 import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { MainPage } from 'pages/MainPage';
-import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@pages/MainPage';
+import { AboutPage } from '@pages/AboutPage';
 import styles from './AppRouter.module.scss';
+import { CompanyMembersPage } from '@pages/CompanyMembersPage';
 
 const AppRouter: FC = () => {
 	return (
@@ -10,6 +11,7 @@ const AppRouter: FC = () => {
 			<Routes>
 				<Route path="/" element={<MainPage />} />
 				<Route path="/AboutUs" element={<AboutPage />} />
+				<Route path="/CompanyMembers" element={<CompanyMembersPage />} />
 			</Routes>
 		</Suspense>
 	);
