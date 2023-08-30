fedd1886ca9ac3ea699d59e136c96e50b272faba
Arteminariy <artemborisoff.ab@gmail.com>
2023-08-30 20:57:34 +0300

    upd: cards

--- src/db/cards.ts
+++ src/db/cards.ts
@@ -7,5 +7,52 @@ export const cards: ICard[] = [
 		img: 'src/assets/images/cards/card_1.jpg',
 		bgi: 'linear-gradient(to left, violet, red)'
 	},
-	
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
+	{
+		title: 'Кчау',
+		text: 'Чекни мать',
+		img: 'src/assets/images/cards/card_1.jpg',
+		bgi: 'linear-gradient(to left, violet, red)'
+	},
 ]
--- src/pages/MainPage/ui/components/MainFavorsCarousel/MainFavorsCarousel.tsx
+++ src/pages/MainPage/ui/components/MainFavorsCarousel/MainFavorsCarousel.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import styles from './MainFavorsCarousel.module.scss'
 import { ICard } from '@/types'
 import Card from './Card/Card'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons'
  
 interface IMainFavorsCarouselProps {
 	cards: ICard[]
@@ -10,11 +11,15 @@ interface IMainFavorsCarouselProps {
 const MainFavorsCarousel: FC<IMainFavorsCarouselProps> = ({cards}) => {
 	return (
 		<section className={styles.FavorsCarouselWrapper}>
+			<ArrowLeftOutlined className={styles.arrow}/>
+			<div className={styles.cardsContainer}>
 			{cards.map(card => {
 				return (
 					<Card card={card}/>
 				)
 			})}
+			</div>
+			<ArrowRightOutlined className={styles.arrow}/>
 		</section>
 	)
 }
