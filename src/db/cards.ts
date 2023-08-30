a50ad8574781ea38b4e606f9e8e9fc5d3a8969a0
Табалов Никита <nikitatabalov@gmail.com>
2023-08-30 23:52:45 +0300

    added: gradient on card

--- src/db/cards.ts
+++ src/db/cards.ts
@@ -1,58 +1,58 @@
-import { ICard } from "@/types";
+import { ICard } from '@/types';
 
 export const cards: ICard[] = [
 	{
 		title: 'Кчау1',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау2',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау3',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау4',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау5',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау6',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау7',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау8',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
 	{
 		title: 'Кчау9',
 		text: 'Чекни мать',
 		img: 'src/assets/images/cards/card_1.jpg',
-		bgi: 'linear-gradient(to left, violet, red)'
+		bgi: 'radial-gradient(circle, rgba(23,29,61,1) 0%, rgba(84,96,158,1) 76%);',
 	},
-]
+];
--- src/pages/MainPage/ui/components/MainFavorsCarousel/Card/Card.tsx
+++ src/pages/MainPage/ui/components/MainFavorsCarousel/Card/Card.tsx
@@ -8,7 +8,10 @@ interface ICardProps {
 
 const Card: FC<ICardProps> = ({ card }) => {
 	return (
-		<div className={styles.CardWrapper} style={{backgroundColor: card.bgc, backgroundImage: card.bgi}}>
+		<div
+			className={styles.CardWrapper}
+			style={{ background: card.bgc, backgroundImage: card.bgi }}
+		>
 			<h4 className={styles.title}>{card.title}</h4>
 			<p className={styles.text}>{card.text}</p>
 			<img className={styles.img} src={card.img} alt={card.title} />
