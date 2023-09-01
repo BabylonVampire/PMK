939fec31379b8fd095a823002d9b4189dd4775e1
Табалов Никита <nikitatabalov@gmail.com>
2023-09-01 20:05:00 +0300

    fix: projectSection

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -1,24 +1,23 @@
-import { IArticle } from "@/types";
+import { IArticle } from '@/types';
 
-export const projects: IArticle[] = [
-	{
-		id: 1,
-		title: 'Каловая ванна',
-		date: {
-			date: 1,
-			month: 'Сентябрь',
-			year: '2023'
+export const projects: IArticle = {
+	id: 1,
+	title: 'Каловая ванна',
+	date: {
+		date: 1,
+		month: 'Сентябрь',
+		year: '2023',
+	},
+	preview:
+		'https://media2.cackle.me/f/e2/753708a07fe7b17aa96679233808ce2f.jpg',
+	content: [
+		{
+			type: 'text',
+			value: 'Насрал',
 		},
-		preview: 'https://media2.cackle.me/f/e2/753708a07fe7b17aa96679233808ce2f.jpg',
-		content: [
-			{
-				type: 'text',
-				value: 'Насрал'
-			},	
-			{
-				type: 'text',
-				value: 'Насрал'
-			}
-		]
-	}
-] 
+		{
+			type: 'text',
+			value: 'Насрал',
+		},
+	],
+};
